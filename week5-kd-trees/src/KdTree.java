d9fbc68cc2b37d74df087c47bc5135d3d4e4aad9
DESKTOP-Q897ULR\Yuen <19308564+wszhan@users.noreply.github.com>
2021-02-26 16:16:40 +0800

    implement brute force. kdtree working for range search (rectangular area). nearest neighbors search to be implemented.

---
+++ week5-kd-trees/src/KdTree.java
@@ -0,0 +1,321 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Stack;
+import java.util.TreeSet;
+
+import org.w3c.dom.css.Rect;
+
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.StdDraw;
+
+public class KdTree {
+
+    private int numberOfPoints;
+    private Node root;
+
+    public KdTree() {
+        this.numberOfPoints = 0;
+        this.root = null;
+    }
+
+    private static class Node {
+        private static final boolean COMPARE_X = true;
+        private static final boolean COMPARE_Y = false;
+
+        private Point2D p;
+        private RectHV rect;
+        private Node lb = null; // left/bottom
+        private Node rt = null; // right/top
+        private boolean comp; 
+
+        Node(Point2D p, boolean compareX, RectHV r) {
+            this.p = p;
+            this.comp = compareX;
+            this.rect = r;
+        }
+    }
+
+    public void draw() {
+        Node curr = root;
+
+        recursiveDraw(curr);
+    }
+
+    private void recursiveDraw(Node node) {
+        if (node != null) {
+            // draw point
+            StdDraw.setPenColor(StdDraw.BLACK);
+            node.p.draw();
+
+            Point2D p1, p2;
+
+            // draw split line
+            if (node.comp == Node.COMPARE_X) {
+                // draw red vertical line
+                p1 = new Point2D(node.p.x(), node.rect.ymin());
+                p2 = new Point2D(node.p.x(), node.rect.ymax());
+                StdDraw.setPenColor(StdDraw.RED);
+            } else {
+                // draw blue horizontal line
+                p1 = new Point2D(node.rect.xmin(), node.p.y());
+                p2 = new Point2D(node.rect.xmax(), node.p.y());
+                StdDraw.setPenColor(StdDraw.BLUE);
+            }
+
+            p1.drawTo(p2);
+
+            recursiveDraw(node.lb);
+            recursiveDraw(node.rt);
+        }
+    }
+
+    public Iterable<Point2D> range(RectHV rect) {
+        if (rect == null) throw new IllegalArgumentException();
+
+        List<Point2D> res = new ArrayList<>();
+
+        traverse(root, res, rect); 
+
+        return res;
+    }
+
+    private void traverse(Node node, List<Point2D> list, RectHV that) {
+        if (node == null || list == null) return ;
+
+        if (that == null) {
+            list.add(node.p);
+        } else {
+            if (node.rect.intersects(that)) {
+                if (that.contains(node.p)) {
+                    list.add(node.p);
+                } else {
+                    // if (node.p.x() == 1.000000 &&
+                        // node.p.y() == 0.500000) {
+                            // System.out.printf(
+                                // "rect: %s\np: %s\ncontain? %b\n",
+                                // that, node.p, that.contains(node.p)
+                            // );
+                        // }
+                }
+                traverse(node.lb, list, that);
+                traverse(node.rt, list, that);
+            }
+        }
+
+    }
+
+
+    public Point2D nearest(Point2D p) {
+        if (p == null) throw new IllegalArgumentException();
+
+        return root != null ? root.p : new Point2D(0.5, 0.5);
+    }
+
+    public boolean contains(Point2D p) {
+        if (p == null) throw new IllegalArgumentException();
+
+        return get(root, p) != null;
+    }
+
+
+    /**
+     * 
+     * @param x
+     * @param p
+     * @return Node if any node contains point p and null otherwise.
+     */
+    private Node get(Node node, Point2D p) {
+        if (node == null) return null;
+
+        int cmp = comparePoint(node, p);
+
+        if (cmp > 0) {
+            return get(node.lb, p);
+        } else {
+            if (cmp < 0 || !node.p.equals(p)) {
+                return get(node.rt, p);
+            }
+        }
+
+        return node;
+    }
+
+
+    public void insert(Point2D p) {
+        if (p == null) throw new IllegalArgumentException();
+
+        root = put(root, p, null, true); // the boolean doesn't matter at all
+    }
+
+    
+    /**
+     * 
+     * @param node  parent node
+     * @param p     for p we compute the rectangle
+     * @param isLB  does p go to the LB branch of the parent node?
+     * @return
+     */
+    private RectHV computeRect(Node node, Point2D p, boolean isLB) {
+        if (node == null) return new RectHV(0.0, 0.0, 1.0, 1.0);
+
+        boolean parentCompareX = node.comp == Node.COMPARE_X;
+        double rectMinX, rectMinY, rectMaxX, rectMaxY;
+
+        double parentNodeRectXMin = node.rect.xmin();
+        double parentNodeRectYMin = node.rect.ymin();
+        double parentNodeRectXMax = node.rect.xmax();
+        double parentNodeRectYMax = node.rect.ymax();
+
+        if (isLB) {
+            if (parentCompareX) {
+                rectMinX = parentNodeRectXMin;
+                rectMinY = parentNodeRectYMin;
+                rectMaxX = node.p.x();
+                rectMaxY = parentNodeRectYMax;
+            } else {
+                rectMinX = parentNodeRectXMin;
+                rectMinY = parentNodeRectYMin;
+                rectMaxX = parentNodeRectXMax;
+                rectMaxY = node.p.y();
+            }
+        } else {
+            if (parentCompareX) {
+                rectMinX = node.p.x();
+                rectMinY = parentNodeRectYMin;
+                rectMaxX = parentNodeRectXMax;
+                rectMaxY = parentNodeRectYMax;
+            } else {
+                rectMinX = parentNodeRectXMin;
+                rectMinY = node.p.y();
+                rectMaxX = parentNodeRectXMax;
+                rectMaxY = parentNodeRectYMax;
+            }
+        }
+        
+        return new RectHV(rectMinX, rectMinY, rectMaxX, rectMaxY);
+    }
+
+    /**
+     * 
+     * @param node the previous node traversed
+     * @param p point2D value to be inserted
+     * @param compareX is node compared with respect to X coordinate?
+     * @return
+     */
+    private Node put(Node node, Point2D p, Node parentNode, boolean isLB) {
+        if (p == null) throw new IllegalArgumentException();
+
+        if (node == null) {
+            boolean compareX = parentNode == null || parentNode.comp == Node.COMPARE_Y;
+
+            this.numberOfPoints++;
+
+            RectHV r = computeRect(parentNode, p, isLB);
+
+            return new Node(p, compareX, r);
+        }
+
+        // compare two nodes based on x/y coordinate
+        int cmp = comparePoint(node, p);
+
+        // recursively search based on comparison result
+        if(cmp > 0) {
+            node.lb = put(node.lb, p, node, cmp > 0);
+        } else if (cmp < 0 || !node.p.equals(p)) {
+            node.rt = put(node.rt, p, node, cmp > 0);
+        }
+
+        return node;
+    }
+
+    /**
+     * Compare point in the current node with the given point.
+     * 
+     * @param node
+     * @param p
+     * @return
+     */
+    private int comparePoint(Node node, Point2D p) {
+        double thisValue, thatValue;
+        int cmp;
+
+        if (node.comp == Node.COMPARE_X) {
+            thisValue = node.p.x();
+            thatValue = p.x();
+        } else {
+            thisValue = node.p.y();
+            thatValue = p.y();
+        }
+
+        if (thisValue > thatValue) cmp = 1;
+        else if (thisValue < thatValue) cmp = -1;
+        else cmp = 0;
+
+        return cmp;
+    }
+
+    public int size() {
+        int n = this.numberOfPoints;
+        return n;
+    }
+
+    public boolean isEmpty() {
+        return this.numberOfPoints == 0;
+    }
+
+    public static void main(String[] args) {
+        KdTree kdt = new KdTree();
+
+        // add point and test size()
+        Point2D p1 = new Point2D(0.5, 0.6);
+        Point2D p2 = new Point2D(0.6, 0.6);
+        Point2D p3 = new Point2D(0.7, 0.8);
+        Point2D p4 = new Point2D(0.7, 0.8);
+
+        assert kdt.isEmpty() == true : "kdt should be empty";
+
+        kdt.insert(p1);
+        assert kdt.isEmpty() == false : "kdt should not be empty";
+        assert kdt.size() == 1 : "should contain only 1 element";
+        assert kdt.contains(new Point2D(p1.x(), p1.y())) == true;
+
+        kdt.insert(p2);
+        assert kdt.size() == 2 : "should contain 2 elements, current size = " + kdt.size();
+        assert kdt.contains(new Point2D(p2.x(), p2.y())) == true;
+
+        kdt.insert(p2);
+        assert kdt.size() == 2 : "should contain 2 elements ";
+
+        kdt.insert(p3);
+        assert kdt.size() == 3;
+        assert kdt.contains(new Point2D(p3.x(), p3.y())) == true;
+
+        kdt.insert(p4);
+        assert kdt.size() == 3;
+
+        // test traverse
+        RectHV rect1 = new RectHV(p1.x(), p1.y(), p2.x(), p2.y());
+        Iterable<Point2D> pointsInRect1 = kdt.range(rect1);
+
+        int n1 = 0;
+        for (Point2D p : pointsInRect1) {
+            if (p.equals(p1) || p.equals(p2)) {
+                n1++;
+            }
+        }
+        assert n1 == 2;
+
+        RectHV rect2 = new RectHV(p1.x(), p1.y(), p3.x(), p3.y());
+        Iterable<Point2D> pointsInRect2 = kdt.range(rect2);
+
+        int n2 = 0;
+        for (Point2D p : pointsInRect2) {
+            if (p.equals(p1) || p.equals(p2) || p.equals(p3)) {
+                n2++;
+            }
+        }
+        assert n2 == 3;
+    }
+}
---
+++ week5-kd-trees/src/PointSET.java
@@ -0,0 +1,111 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeSet;
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+public class PointSET {
+
+    private TreeSet<Point2D> points;
+
+    public PointSET() {
+        points = new TreeSet<Point2D>();
+    }
+
+    public void draw() {
+        for (Point2D p : points) {
+            p.draw();
+        }
+    }
+
+    public Iterable<Point2D> range(RectHV rect) {
+        if (rect == null) throw new IllegalArgumentException();
+
+        List<Point2D> res = new ArrayList<>();
+
+        double rxmin = rect.xmin();
+        double rymin = rect.ymin();
+        double rxmax = rect.xmax();
+        double rymax = rect.ymax();
+
+        for (Point2D p : points) {
+            // point p falls in the rectangular range?
+            if (p.x() >= rxmin && 
+                p.x() <= rxmax &&
+                p.y() >= rymin &&
+                p.y() <= rymax) {
+                    res.add(p);
+                }
+        }
+
+        return res;
+    }
+
+    public Point2D nearest(Point2D p) {
+        if (p == null) throw new IllegalArgumentException();
+
+        double minDistanceSquared = Double.MAX_VALUE;
+        Point2D closest = null;
+
+        for (Point2D candidate : points) {
+            double dist = p.distanceSquaredTo(candidate);
+            if (dist < minDistanceSquared) {
+                minDistanceSquared = dist;
+                closest = candidate;
+            }
+        }
+
+        return closest;
+    }
+
+    public boolean contains(Point2D p) {
+        if (p == null) throw new IllegalArgumentException();
+
+        return points.contains(p);
+    }
+
+    public void insert(Point2D p) {
+        if (p == null) throw new IllegalArgumentException();
+
+        points.add(p);
+    }
+
+    public int size() {
+        return points.size();
+    }
+
+    public boolean isEmpty() {
+        return points.size() == 0;
+    }
+
+    public static void main(String[] args) {
+        PointSET ps = new PointSET();
+
+        // add point and test size()
+        Point2D p1 = new Point2D(0.5, 0.6);
+        Point2D p2 = new Point2D(0.6, 0.6);
+        Point2D p3 = new Point2D(0.7, 0.8);
+        Point2D p4 = new Point2D(0.7, 0.8);
+
+        assert ps.isEmpty() == true : "ps should be empty";
+
+        ps.insert(p1);
+        assert ps.isEmpty() == false : "ps should not be empty";
+        assert ps.size() == 1 : "should contain only 1 element";
+        assert ps.contains(new Point2D(p1.x(), p1.y())) == true;
+
+        ps.insert(p2);
+        assert ps.size() == 2 : "should contain 2 elements";
+        assert ps.contains(new Point2D(p2.x(), p2.y())) == true;
+
+        ps.insert(p2);
+        assert ps.size() == 2 : "should contain 2 elements ";
+
+        ps.insert(p3);
+        assert ps.size() == 3;
+        assert ps.contains(new Point2D(p3.x(), p3.y())) == true;
+
+        ps.insert(p4);
+        assert ps.size() == 3;
+    }
+}
